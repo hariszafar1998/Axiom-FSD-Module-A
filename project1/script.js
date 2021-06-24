50a3ff1c90da782ebd9d7c9135f0bd632765b916
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-06-25 01:32:19 +0500

    event listner added

---
+++ project1/script.js
@@ -0,0 +1,12 @@
+const form=document.getElementById('form');
+const username=document.getElementById('username');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const password2=document.getElementById('password2');
+
+
+// Event listner for button
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
