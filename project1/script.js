914dd6ae353daa9305d9c660a600951a3c3f2cd6
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-06-25 02:03:49 +0500

    required fields

--- project1/script.js
+++ project1/script.js
@@ -4,10 +4,49 @@ const email=document.getElementById('email');
 const password=document.getElementById('password');
 const password2=document.getElementById('password2');
 
+// function to show error
+
+function showError(input,message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// function to show success
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'
+}
 
 // Event listner for button
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log(username.value);
+
+    if (username.value===''){
+        showError(username,'Username is required');
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value===''){
+        showError(email,'Email is required');
+    } else {
+        showSuccess(email);
+    }
+
+    if (password.value===''){
+        showError(password,'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value===''){
+        showError(password2,'Password Confirmation is required');
+    } else {
+        showSuccess(password2);
+    }
+
 })
