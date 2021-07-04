91620bb12f00edd322416a6489d3a52e349c69e1
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-07-04 16:48:27 +0500

    chnage

--- project4/script.js
+++ project4/script.js
@@ -1,6 +1,18 @@
 const currencyOne = document.getElementById('currencyOne');
 
-const rate = fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/latest/${currencyOne}`);
+const rate = fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/pair/${currencyOne}`)
+.then(res => res.json())
+.then(data => {
+    console.log(data.conversion_rate);
+    // Get the Conversion Rate from Currency One to Currency Two
+    const conversionRate = data.conversion_rate;
+    // Update the DOM to display the conversion rate
+    rate.innerText = `1 ${currencyOneCode} = ${conversionRate} ${currencyTwoCode}`;
+    // Formatting Currency Two Amount
+    const amount2 = new Intl.NumberFormat('en-US', { style: 'currency', currency: currencyTwoCode }).format((amountCurrencyOne.value * conversionRate).toFixed(2));
+    // Updating DOM
+    amountCurrencyTwo.value = amount2;
+});
 
 // Functions
 
