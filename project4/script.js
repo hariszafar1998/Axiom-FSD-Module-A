e90bce3177ff96f668952f5429eda86c5e3dcd93
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-07-06 14:32:23 +0500

    changes

--- project4/script.js
+++ project4/script.js
@@ -2,26 +2,25 @@ const currencyOne = document.getElementById('currencyOne');
 const currencyTwo = document.getElementById('currencyTwo');
 
 
-const rate = fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/pair/${currencyOne}/${currencyTwo}`)
-.then(res => res.json())
-.then(data => {
-    console.log(data.conversion_rate);
-    // Get the Conversion Rate from Currency One to Currency Two
-    const conversionRate = data.conversion_rate;
-    // Update the DOM to display the conversion rate
-    rate.innerText = `1 ${currencyOneCode} = ${conversionRate} ${currencyTwoCode}`;
-    // Formatting Currency Two Amount
-    const amount2 = new Intl.NumberFormat('en-US', { style: 'currency', currency: currencyTwoCode }).format((amountCurrencyOne.value * conversionRate).toFixed(2));
-    // Updating DOM
-    amountCurrencyTwo.value = amount2;
-});
+// Fetch Exchange Rates & Update the DOM
+function calculate() {
+    // Get the Currency Code for currency 1 and 2
+    const currencyOneCode = currencyOne.value;
+    const currencyTwoCode = currencyTwo.value;
 
-// Functions
+    // Send Request to ExchangeRate-API for conversion rates for currency one
+    fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/pair/${currencyOneCode}/${currencyTwoCode}`)
+        .then(res => res.json())
+        .then(data => {
+            // Get the Conversion Rate from Currency One to Currency Two
+            const conversionRate = data.conversion_rate;
+            console.log(conversionRate);
+        });
+};
 
 function changeInCurrencyOne() {
-    console.log(rate);
 };
 
 // Event listners
 
-currencyOne.addEventListener('change', changeInCurrencyOne);
+currencyOne.addEventListener('change', calculate);
