793e46f394b222c1d00a47c59c561e1908f98a9f
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-07-06 16:11:08 +0500

    project 4 done

--- project4/script.js
+++ project4/script.js
@@ -1,26 +1,59 @@
-const currencyOne = document.getElementById('currencyOne');
-const currencyTwo = document.getElementById('currencyTwo');
+const currencyOne = document.getElementById('currencyOneSelect');
+const currencyTwo = document.getElementById('currencyTwoSelect');
+const currencyOneAmount = document.getElementById('amountOne');
+const currencyTwoAmount = document.getElementById('amountTwo');
+const rateUpdate = document.getElementById('rate');
+const swap = document.getElementById('swap');
 
+// Functions
 
-// Fetch Exchange Rates & Update the DOM
 function calculate() {
-    // Get the Currency Code for currency 1 and 2
     const currencyOneCode = currencyOne.value;
     const currencyTwoCode = currencyTwo.value;
 
-    // Send Request to ExchangeRate-API for conversion rates for currency one
-    fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/pair/${currencyOneCode}/${currencyTwoCode}`)
-        .then(res => res.json())
-        .then(data => {
-            // Get the Conversion Rate from Currency One to Currency Two
-            const conversionRate = data.conversion_rate;
-            console.log(conversionRate);
-        });
+    // fetch data from API
+
+    fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/latest/${currencyOneCode}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const conversionRateOne = data.conversion_rates[currencyTwoCode];
+        rateUpdate.innerText = `1 ${currencyOneCode} = ${conversionRateOne} ${currencyTwoCode}`;
+        currencyTwoAmount.value = currencyOneAmount.value * conversionRateOne;
+    })
 };
 
-function changeInCurrencyOne() {
+// Functions
+
+function calculateAgain() {
+    const currencyOneCode = currencyOne.value;
+    const currencyTwoCode = currencyTwo.value;
+
+    // fetch data from API
+
+    fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/latest/${currencyTwoCode}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const conversionRateTwo = data.conversion_rates[currencyOneCode];
+        currencyOneAmount.value = currencyTwoAmount.value * conversionRateTwo;
+    })
 };
 
-// Event listners
+function swapCurrencies() {
+    const saveCurrenyOneValue = currencyOne.value;
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = saveCurrenyOneValue;
+
+    const saveOneRate = currencyOneAmount.value;
+    currencyOneAmount.value = currencyTwoAmount.value;
+    currencyTwoAmount.value = saveOneRate;
+};
+
+// Event Listeners
 
 currencyOne.addEventListener('change', calculate);
+currencyTwo.addEventListener('change',calculate);
+currencyOneAmount.addEventListener('input',calculate);
+currencyTwoAmount.addEventListener('input',calculateAgain);
+swap.addEventListener('click', swapCurrencies);
+
+calculate();
