df901dd60c270e33efd766f9bbeb04aee8e95eaf
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-07-10 23:44:45 +0500

    pair conversion done

--- project4/script.js
+++ project4/script.js
@@ -4,7 +4,7 @@ const amountOne = document.getElementById('amountOne');
 const amountTwo = document.getElementById('amountTwo');
 const swap = document.getElementById('swap');
 const rate = document.getElementById('rate');
-
+const start = document.getElementById('start');
 
 
 // Functions
@@ -13,10 +13,12 @@ function calculate() {
     currencyOneCode = currencyOne.value;
     currencyTwoCode = currencyTwo.value;
 
-    fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/latest/${currencyOneCode}`)
+    fetch(`https://v6.exchangerate-api.com/v6/2c776b4e99e3b430fdf66795/pair/${currencyOneCode}/${currencyTwoCode}`)
     .then((response) => response.json())
     .then((data) => {
-        const conversionRate = data.conversion_rates[currencyTwoCode];
+        console.log(data);
+        const conversionRate = (data.conversion_rate).toFixed(2);
+        // console.log(conversionRate);
         rate.innerText = `1 ${currencyOneCode} = ${conversionRate} ${currencyTwoCode}`;
         amountTwo.value = amountOne.value * conversionRate;
     })
@@ -36,4 +38,5 @@ function swapCurrencies() {
 // currencyOne.addEventListener('change', calculate);
 // currencyTwo.addEventListener('change', calculate);
 // amountOne.addEventListener('input', calculate);
-// swap.addEventListener('click', swapCurrencies);
+swap.addEventListener('click', swapCurrencies);
+start.addEventListener('click', calculate);
