c2ebba171ca99806e04c726ccb8ec829e816707e
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-07-12 16:46:30 +0500

    first practice done

--- project5/script.js
+++ project5/script.js
@@ -7,48 +7,76 @@ const main = document.getElementById('main');
 
 let userArray = [];
 
-
-// function to fetch the name from API
-
-function getData() {
+function generateRandomUser() {
     fetch('https://randomuser.me/api/')
-        .then(res => res.json())
+        .then(response => response.json())
         .then(data => {
+            const fetchedName = data.results[0].name; 
+            const wealth = Math.floor(Math.random() * 1000000);
+            
+            const newUser = {name:`${fetchedName.title} ${fetchedName.first} ${fetchedName.last}`, 
+                            wealth: wealth}
 
-            const userWealth = Math.floor(Math.random() * 1000000);
+            // console.log(newUser);
 
-            userWealthNew = userWealth.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-            const nameData = data.results[0];
-            const newUser = {
-                name: `${nameData.name.title} ${nameData.name.first} ${nameData.name.last}`, 
-                Wealth: userWealthNew
-            }
-
-            addUserData(newUser);
-
-            // console.log(User);
-
-            // updateDOM();
+            insertUserInTheArray(newUser);
         })
 };
 
-function addUserData(user) {
+function insertUserInTheArray(user) {
     userArray.push(user);
     // console.log(userArray);
     updateDOM();
 };
 
-function updateDOM(newArray = userArray) {
-    console.log(newArray);
-    const userNameAndWealthDiv = document.createElement('div');
-    userNameAndWealthDiv.classList.add('user');
-    userNameAndWealthDiv.innerHTML = `<strong>${newArray[0].name}</strong>$${newArray[0].Wealth}`;
+function updateDOM(userData = userArray) {
+    main.innerHTML = '<h2><strong>User</strong>Wealth</h2>';
 
-    main.appendChild(userNameAndWealthDiv);
-
-    // console.log(userName);
-    // console.log(userWealth);
+    userData.forEach( user  => {
+        const newUserDiv = document.createElement('div');
+        newUserDiv.classList.add('user');
+        newUserDiv.innerHTML = `<strong>${user.name}</strong>$${formatWealth(user.wealth)}`;
+        main.appendChild(newUserDiv);
+    });
 };
 
-addUser.addEventListener('click', getData)
+function formatWealth(wealth) {
+    return wealth.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+};
 
+function doubleUsersWealth() {
+    userArray = userArray.map( user => {
+        return {...user, wealth: user.wealth *2}
+    });
+    updateDOM();
+};
+
+function filterMillionairs() {
+    userArray = userArray.filter( user => user.wealth > 1000000 );
+    updateDOM();
+};
+
+function sortMillionairs() {
+    userArray = userArray.sort((a, b) => b.wealth - a.wealth);
+    updateDOM();
+};
+
+function doTotal() {
+    const netWealth = userArray.reduce(( acc , user ) => 
+        (acc += user.wealth), 0
+    );
+    const newTotalDiv = document.createElement('div');
+    newTotalDiv.classList.add('totalDiv');
+    newTotalDiv.innerHTML = `<strong>Net Wealth: $${formatWealth(netWealth)}</strong>`;
+    main.appendChild(newTotalDiv);
+};
+
+addUser.addEventListener('click', generateRandomUser);
+doubleWealth.addEventListener('click', doubleUsersWealth);
+showMillionairs.addEventListener('click', filterMillionairs);
+sortUsers.addEventListener('click', sortMillionairs);
+calculateNetWealth.addEventListener('click', doTotal);
+
+generateRandomUser();
+generateRandomUser();
+generateRandomUser();
