e65891173eea648aa2700beb78fe64b4ffbf4dfb
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-08-25 01:29:58 +0500

    done

--- project9/script.js
+++ project9/script.js
@@ -6,74 +6,149 @@ const transactionForm = document.getElementById('transactionform');
 const amountAdd = document.getElementById('amount');
 const reasonAdd = document.getElementById('reason');
 
-const transactions = [];
+if ( localStorage.getItem('transactionData') ) {
+    const transactions = JSON.parse(localStorage.getItem('transactionData'));
 
-let transactionArray = transactions;
+    let transactionArray = transactions;
 
-function init() {
-    const transactionAmounts = transactionArray.map( transaction => transaction.amount )
-    // console.log(transactionAmounts);
-    
-    totalBalance.innerText = `$${formatWealth(transactionAmounts.reduce( (acc, amount) => (acc += amount) , 0))}`;
-    creditBalance.innerText = `$${formatWealth(transactionAmounts.filter( amount => amount > 0 ).reduce( (acc, amount) => ( acc += amount ), 0 ))}`;
-    debitBalance.innerText = `$${formatWealth(transactionAmounts.filter( amount => amount < 0 ).reduce( (acc, amount) => ( acc += amount ), 0 ))}`;
-    
-    displayHistory(transactionArray);
-};
+    function init() {
+        const transactionAmounts = transactionArray.map( transaction => transaction.amount )
+        // console.log(transactionAmounts);
+        
+        totalBalance.innerText = `$${formatWealth(transactionAmounts.reduce( (acc, amount) => (acc += amount) , 0))}`;
+        creditBalance.innerText = `$${formatWealth(transactionAmounts.filter( amount => amount > 0 ).reduce( (acc, amount) => ( acc += amount ), 0 ))}`;
+        debitBalance.innerText = `$${formatWealth(transactionAmounts.filter( amount => amount < 0 ).reduce( (acc, amount) => ( acc += amount ), 0 ))}`;
+        
+        displayHistory(transactionArray);
+    };
 
-function displayHistory(transactionArrayNew) {
-    historyList.innerHTML = '';
-    transactionArrayNew.map ( transaction => {
-        const transactionType = JSON.stringify(transaction.amount).split('');
-        // console.log(transactionType);
-        if ( transactionType.includes('-') ) {
-            const newListItem = document.createElement('li');
-            newListItem.classList.add('list-item-debit');
-            newListItem.innerHTML = `<span id="reason">${transaction.reason}</span> <span id="amount-debit">$${formatWealth(transaction.amount)}</span> <button class="deletebtn" id="deletebtn" onclick="deleteTransaction(${transaction.id})">X</button>`;
-            historyList.appendChild(newListItem);
+    function displayHistory(transactionArrayNew) {
+        historyList.innerHTML = '';
+        transactionArrayNew.map ( transaction => {
+            const transactionType = JSON.stringify(transaction.amount).split('');
+            // console.log(transactionType);
+            if ( transactionType.includes('-') ) {
+                const newListItem = document.createElement('li');
+                newListItem.classList.add('list-item-debit');
+                newListItem.innerHTML = `<span id="reason">${transaction.reason}</span> <span id="amount-debit">$${formatWealth(transaction.amount)}</span> <button class="deletebtn" id="deletebtn" onclick="deleteTransaction(${transaction.id})">X</button>`;
+                historyList.appendChild(newListItem);
+            } else {
+                const newListItem = document.createElement('li');
+                newListItem.classList.add('list-item-credit');
+                newListItem.innerHTML = `<span id="reason">${transaction.reason}</span> <span id="amount-debit">$${formatWealth(transaction.amount)}</span> <button class="deletebtn" id="deletebtn" onclick="deleteTransaction(${transaction.id})">X</button>`;
+                historyList.appendChild(newListItem);
+            }
+        });
+    };
+
+    function formatWealth(amount) {
+        return amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    };
+
+    function getID() {
+        return Math.floor(Math.random()*100000000);
+    };
+
+    function formHandler(e) {
+        e.preventDefault();
+        
+        if ( amountAdd.value && reasonAdd.value ) {
+            const newObject = {
+                id: getID(),
+                reason: reasonAdd.value,
+                amount: +amountAdd.value
+            }
+            // console.log(+amount.value);
+            transactionArray.push(newObject);
         } else {
-            const newListItem = document.createElement('li');
-            newListItem.classList.add('list-item-credit');
-            newListItem.innerHTML = `<span id="reason">${transaction.reason}</span> <span id="amount-debit">$${formatWealth(transaction.amount)}</span> <button class="deletebtn" id="deletebtn" onclick="deleteTransaction(${transaction.id})">X</button>`;
-            historyList.appendChild(newListItem);
+            alert('Please enter the transaction amount or reason.');
         }
-    });
-};
+        init();
+        amountAdd.value = '';
+        reasonAdd.value = '';
+        console.log(transactionArray);
+        localStorage.setItem('transactionData', JSON.stringify(transactionArray));
+    };
 
-function formatWealth(amount) {
-    return amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-};
+    function deleteTransaction(id) {
+        transactionArray = transactionArray.filter( transaction => transaction.id !== id );
+        init();
+    };
 
-function getID() {
-    return Math.floor(Math.random()*100000000);
-};
+    transactionForm.addEventListener('submit', formHandler)
 
-function formHandler(e) {
-    e.preventDefault();
-    
-    if ( amountAdd.value && reasonAdd.value ) {
-        const newObject = {
-            id: getID(),
-            reason: reasonAdd.value,
-            amount: +amountAdd.value
+    init();
+} else {
+    const transactions = [];
+
+    let transactionArray = transactions;
+
+    function init() {
+        const transactionAmounts = transactionArray.map( transaction => transaction.amount )
+        // console.log(transactionAmounts);
+        
+        totalBalance.innerText = `$${formatWealth(transactionAmounts.reduce( (acc, amount) => (acc += amount) , 0))}`;
+        creditBalance.innerText = `$${formatWealth(transactionAmounts.filter( amount => amount > 0 ).reduce( (acc, amount) => ( acc += amount ), 0 ))}`;
+        debitBalance.innerText = `$${formatWealth(transactionAmounts.filter( amount => amount < 0 ).reduce( (acc, amount) => ( acc += amount ), 0 ))}`;
+        
+        displayHistory(transactionArray);
+    };
+
+    function displayHistory(transactionArrayNew) {
+        historyList.innerHTML = '';
+        transactionArrayNew.map ( transaction => {
+            const transactionType = JSON.stringify(transaction.amount).split('');
+            // console.log(transactionType);
+            if ( transactionType.includes('-') ) {
+                const newListItem = document.createElement('li');
+                newListItem.classList.add('list-item-debit');
+                newListItem.innerHTML = `<span id="reason">${transaction.reason}</span> <span id="amount-debit">$${formatWealth(transaction.amount)}</span> <button class="deletebtn" id="deletebtn" onclick="deleteTransaction(${transaction.id})">X</button>`;
+                historyList.appendChild(newListItem);
+            } else {
+                const newListItem = document.createElement('li');
+                newListItem.classList.add('list-item-credit');
+                newListItem.innerHTML = `<span id="reason">${transaction.reason}</span> <span id="amount-debit">$${formatWealth(transaction.amount)}</span> <button class="deletebtn" id="deletebtn" onclick="deleteTransaction(${transaction.id})">X</button>`;
+                historyList.appendChild(newListItem);
+            }
+        });
+    };
+
+    function formatWealth(amount) {
+        return amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    };
+
+    function getID() {
+        return Math.floor(Math.random()*100000000);
+    };
+
+    function formHandler(e) {
+        e.preventDefault();
+        
+        if ( amountAdd.value && reasonAdd.value ) {
+            const newObject = {
+                id: getID(),
+                reason: reasonAdd.value,
+                amount: +amountAdd.value
+            }
+            // console.log(+amount.value);
+            transactionArray.push(newObject);
+        } else {
+            alert('Please enter the transaction amount or reason.');
         }
-        // console.log(+amount.value);
-        transactionArray.push(newObject);
-    } else {
-        alert('Please enter the transaction amount or reason.');
-    }
+        init();
+        amountAdd.value = '';
+        reasonAdd.value = '';
+        console.log(transactionArray);
+        localStorage.setItem('transactionData', JSON.stringify(transactionArray));
+    };
+
+    function deleteTransaction(id) {
+        transactionArray = transactionArray.filter( transaction => transaction.id !== id );
+        init();
+    };
+
+    transactionForm.addEventListener('submit', formHandler)
+
     init();
-    amountAdd.value = '';
-    reasonAdd.value = '';
-    console.log(transactionArray);
-    localStorage.setItem('data', transactionArray)
-};
+}
 
-function deleteTransaction(id) {
-    transactionArray = transactionArray.filter( transaction => transaction.id !== id );
-    init();
-};
-
-transactionForm.addEventListener('submit', formHandler)
-
-init();
