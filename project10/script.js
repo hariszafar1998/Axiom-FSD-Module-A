8302b0ee02f975c0cfc2c3379f1dafbe4a79f5e2
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-09-03 01:58:42 +0500

    working

---
+++ project10/script.js
@@ -0,0 +1,12 @@
+const mainContainer = document.getElementById('container');
+const track = document.getElementById('audio');
+const albumArt = document.getElementById('albumart');
+const trackTitle = document.getElementById('tracktitle');
+const progressBar = document.getElementById('progressbar');
+const backwardBtn = document.getElementById('backwardbtn');
+const playBtn = document.getElementById('playbtn');
+const forwardBtn = document.getElementById('forwardbtn');
+
+let tracks = ['Ertugrul','National Anthem'];
+
+let trackIndex = 1;
