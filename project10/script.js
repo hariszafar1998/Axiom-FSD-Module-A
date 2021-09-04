a9f366e1a2bd358e0aeb88dd1e3b155a231a5321
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-09-04 20:32:49 +0500

    done

--- project10/script.js
+++ project10/script.js
@@ -6,6 +6,7 @@ const progressBar = document.getElementById('progressbar');
 const backwardBtn = document.getElementById('backwardbtn');
 const playBtn = document.getElementById('playbtn');
 const forwardBtn = document.getElementById('forwardbtn');
+const progressConatiner = document.getElementById('progresscontainer');
 
 let tracksArray = ['Ertugrul','National Anthem'];
 
@@ -31,6 +32,39 @@ function pauseTrack() {
     audio.pause();
 };
 
+function nextTrack() {
+    trackIndex++;
+    if ( trackIndex > tracksArray.length - 1 ) {
+        trackIndex = 0;
+    };
+    loadTrack(tracksArray[trackIndex]);
+    playTrack();
+};
+
+function prevTrack() {
+    trackIndex--;
+    if ( trackIndex < 0 ) {
+        trackIndex = tracksArray.length - 1 ;
+    };
+    loadTrack(tracksArray[trackIndex]);
+    playTrack();
+};
+
+function updateProgress(e) {
+    const { duration , currentTime } = e.srcElement;
+    // console.log( duration , currentTime );
+    progressBarWidth = ( currentTime / duration ) * 100 ;
+    console.log(progressBarWidth);
+    progressBar.style.width = `${progressBarWidth}%`;
+};
+
+function updateTrackProgress(e) {
+    const totalWidth = this.clientWidth;
+    const distanceX = e.offsetX;
+    const audioDuration = audio.duration;
+    audio.currentTime = ( distanceX / totalWidth ) * audioDuration;
+};
+
 playBtn.addEventListener('click', () => {
     const isTrackPlaying = mainContainer.classList.contains('play')
     if ( isTrackPlaying ) {
@@ -40,6 +74,14 @@ playBtn.addEventListener('click', () => {
     }
 });
 
-forwardBtn.addEventListener('click', nextTrack)
+forwardBtn.addEventListener('click', nextTrack);
+
+backwardBtn.addEventListener('click', prevTrack);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+audio.addEventListener('ended', nextTrack);
+
+progressConatiner.addEventListener('click', updateTrackProgress);
 
 loadTrack(tracksArray[trackIndex]);
