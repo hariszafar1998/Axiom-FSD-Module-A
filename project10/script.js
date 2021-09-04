64e7f17283a3e39490842c173f3a9f91e791bee7
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-09-04 19:50:51 +0500

    ...

--- project10/script.js
+++ project10/script.js
@@ -1,5 +1,5 @@
 const mainContainer = document.getElementById('container');
-const track = document.getElementById('audio');
+const audio = document.getElementById('audio');
 const albumArt = document.getElementById('albumart');
 const trackTitle = document.getElementById('tracktitle');
 const progressBar = document.getElementById('progressbar');
@@ -7,6 +7,37 @@ const backwardBtn = document.getElementById('backwardbtn');
 const playBtn = document.getElementById('playbtn');
 const forwardBtn = document.getElementById('forwardbtn');
 
-let tracks = ['Ertugrul','National Anthem'];
+let tracksArray = ['Ertugrul','National Anthem'];
 
 let trackIndex = 1;
+
+function loadTrack(track) {
+    trackTitle.innerText = track;
+    audio.src = `music/${track}.mp3`;
+    albumArt.src = `images/${track}.jpeg`;
+};
+
+function playTrack() {
+    mainContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+};
+
+function pauseTrack() {
+    mainContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    audio.pause();
+};
+
+playBtn.addEventListener('click', () => {
+    const isTrackPlaying = mainContainer.classList.contains('play')
+    if ( isTrackPlaying ) {
+        pauseTrack();
+    } else {
+        playTrack();
+    }
+});
+
+loadTrack(tracksArray[trackIndex]);
