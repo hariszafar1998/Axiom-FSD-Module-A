041bce8b4771f0ad743fe9fe0932e45390780a6d
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-10-10 21:25:18 -0700

    ...
    ...

--- project12/script.js
+++ project12/script.js
@@ -1,20 +1,47 @@
-const score = document.getElementById('score');
-const time = document.getElementById('time');
-const generatedWord = document.getElementById('word');
+const scoreElement = document.getElementById('score');
+const timeElement = document.getElementById('time');
+const wordElement = document.getElementById('word');
 const userInput = document.getElementById('userentry');
 const settingsBtn = document.getElementById('settingsbtn');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelector = document.getElementById('difficulty');
 
+let score = 0;
+let time = 100;
+userInput.focus();
+
 function fetchWord() {
     fetch('https://random-words-api.vercel.app/word')
         .then( res => res.json() )
-        .then( data => {
-            console.log(data);
-        })
-}
+        .then ( data => {
+            const ranW = data[0].word;
+            displayWord(ranW.toLowerCase())
+        } )
+};
 
-fetchWord()
+fetchWord();
+
+function displayWord(randomWord) {
+    wordElement.innerText = randomWord;
+    gameProcess(randomWord);
+};
+
+function gameProcess(word) {
+    userInput.addEventListener('input', e => {
+        if ( e.target.value == word ) {
+            score++;
+            scoreElement.innerText = score;
+            userInput.value = '';
+            fetchWord();
+        }
+    });
+};
+
+const timeInterval = setInterval(() => {
+    time--;
+    timeElement.innerText = time;
+}, 1000);
+
 
 settingsBtn.addEventListener('click', e => {
     settingsForm.classList.toggle('show');
